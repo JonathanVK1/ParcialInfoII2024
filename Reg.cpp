3d827dc8ad210cfa4a1508a64b280d78b8057198
JonathanVK1 <jonathanvictoria41@gmail.com>
2024-05-27 21:48:24 -0300

    Correccion de la libreria getRegister

--- Reg.cpp
+++ Reg.cpp
@@ -12,52 +12,52 @@ void getRegister(uint64_t header){
     uint16_t Low=(header<<16)>>48;
     printf("Lower Level : %u\n",Low);
 
-    uint8_t type=(header <<38)>>62;
+    uint8_t type=(header <<38)>>61;
 
     uint16_t Upper=(header<<48)>>48;
     if(type==0){
-        printf("Devide Type %u\t",type);
+        printf("Device Type %u\t",type);
         printf("Info : ignorar\t");
         printf("UpperLevel : %u\n",Upper);
 
     }
     if(type==3){
-        printf("Devide Type %u\t",type);
+        printf("Device Type %u\t",type);
         printf("Info : ignorar\t");
         printf("UpperLevel : %u\n",Upper);
     }
     if(type==1){
-        uint8_t Sens=(header<<44)>>62;
+        uint8_t Sens=(header<<44)>>61;
         if(Sens==0){
-            printf("Devide Type %u\t",type);
+            printf("Device Type %u\t",type);
             printf("Info : Flow\t",Sens);
             printf("UpperLevel : %u\n",Upper);
         }
         if(Sens==1){
-            printf("Devide Type %u\t",type);
+            printf("Device Type %u\t",type);
             printf("Info : Temp\t",Sens);
             printf("UpperLevel : %u\n",Upper);
         }
         if(Sens==2){
-            printf("Devide Type %u\t",type);
+            printf("Device Type %u\t",type);
             printf("Info : Presure\t",Sens);
             printf("UpperLevel : %u\n",Upper);
         }
         if(Sens==3){
-            printf("Devide Type %u\t",type);
+            printf("Device Type %u\t",type);
             printf("Info : Level\t",Sens);
             printf("UpperLevel : %u\n",Upper);
         }
     }
     if(type==2){
-        uint8_t Act=(header<<40)>>63;
+        uint8_t Act=(header<<40)>>62;
         if(Act==1){
-            printf("Devide Type %u\t",type);
+            printf("Device Type %u\t",type);
             printf("Info : Valve\t",Act);
             printf("UpperLevel : %u\n",Upper);
         }
         if(Act==2){
-            printf("Devide Type %u\t",type);
+            printf("Device Type %u\t",type);
             printf("Info : Motor\t",Act);
             printf("UpperLevel : %u\n",Upper);
         }
